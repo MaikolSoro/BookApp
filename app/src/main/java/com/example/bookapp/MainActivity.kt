2a6adf894496a7390eafa2ee151115d854d97246
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-09-22 18:16:53 -0600

    Create TextInputView (Search)

--- app/src/main/java/com/example/bookapp/MainActivity.kt
+++ app/src/main/java/com/example/bookapp/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.bookapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bookapp.components.ItemBookList
+import com.example.bookapp.components.TextInputField
 import com.example.bookapp.ui.theme.BookAppTheme
 
+
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +23,23 @@ class MainActivity : ComponentActivity() {
             BookAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    ItemBookList()
+                    val input = remember {
+                        mutableStateOf("")
+                    }
+                    LazyColumn() {
+                        item {
+                            TextInputField(
+                                label = "Search for books...",
+                                value = input.value,
+                                onValueChanged = {
+                                    input.value = it
+                                })
+                        }
+                        item {
+                            ItemBookList()
+                        }
+                    }
+
                 }
             }
         }
